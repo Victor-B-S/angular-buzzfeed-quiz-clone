969e05bf920286dcc582bc5ddee18bdf63ae9a47
Victor Bittencourt da Silva <victor-b.s@hotmail.com>
2023-04-08 14:11:40 -0300

    CSS mais responsivo

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -34,9 +34,6 @@ export class QuizComponent implements OnInit {
 
       this.questionIndex = 0
       this.questionMaxIndex = this.questions.length
-
-      console.log(this.questionIndex)
-      console.log(this.questionMaxIndex)
     }
 
   }
